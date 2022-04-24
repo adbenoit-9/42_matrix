4e574d8ddf99fb0d8df6fd7b095e9f4ed46a0e07
Adeline BENOIT <adbenoit@e2r6p9.clusters.42paris.fr>
2022-04-24 21:15:49 +0200

    ex05 done

--- ex05/angle_cos.cpp
+++ ex05/angle_cos.cpp
@@ -2,9 +2,37 @@
 #include "../Matrix.hpp"
 
 template<typename T>
-double   angle_cos<T>(Vector<T> &u, Vector<T> &v) {
-    
+T   angle_cos(Vector<T> &u, Vector<T> &v) {
+    return u.dot(v) / (u.norm() * v.norm());
 }
 
 int main() {
+    Vector<double> u = {1., 0.};
+    Vector<double> v = {1., 0.};
+    std::cout   << "angle_cos({1., 0.}, {1., 0.}) =\n"
+                << angle_cos<double>(u, v) << std::endl
+                << std::endl;
+
+    u = {1., 0.};
+    v = {0., 1.};
+    std::cout   << "angle_cos({1., 0.}, {0., 1.}) =\n"
+                << angle_cos<double>(u, v) << std::endl
+                << std::endl;
+
+    u = {-1., 1.};
+    v = {1., -1.};
+    std::cout   << "angle_cos({-1., 1.}, {1., -1.}) =\n"
+                << angle_cos<double>(u, v) << std::endl
+                << std::endl;
+
+    u = {2., 1.};
+    v = {4., 2.};
+    std::cout   << "angle_cos({2., 1.}, {4., 2.}) =\n"
+                << angle_cos<double>(u, v) << std::endl
+                << std::endl;
+
+    u = {1., 2., 3.};
+    v = {4., 5., 6.};
+    std::cout   << "angle_cos({1., 2., 3.}, {4., 5., 6.}) =\n"
+                << angle_cos<double>(u, v) << std::endl;
 }
