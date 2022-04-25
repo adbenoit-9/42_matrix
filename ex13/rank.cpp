a40d981f236c5c3bfd739b6eda661193197b160f
Adeline BENOIT <adbenoit@e1r5p10.clusters.42paris.fr>
2022-04-25 19:14:40 +0200

    ex13 ok mais depend de 10 qui ko

---
+++ ex13/rank.cpp
@@ -0,0 +1,27 @@
+#include "../Vector.hpp"
+#include "../Matrix.hpp"
+
+int main() {
+    Matrix<double> u;
+    u = {
+        {1., 0., 0.},
+        {0., 1., 0.},
+        {0., 0., 1.}
+    };
+    std::cout << u.rank() << std::endl;
+
+    u = {
+        {1., 2., 0., 0.},
+        {2., 4., 0., 0.},
+        {-1., 2., 1., 1.}
+    };
+    std::cout << u.rank() << std::endl;
+
+    u = {
+        {8., 5., -2.},
+        {4., 7., 20.},
+        {7., 6., 1.},
+        {21., 18., 7.}
+    };
+    std::cout << u.rank() << std::endl;
+}
